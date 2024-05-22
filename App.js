4db53684ee7974b66d41ac8e77d1f51e53987c3f
jeons <etoile929@naver.com>
2024-05-22 23:49:11 +0900

    갤러리 프로젝트 환경설정

--- App.js
+++ App.js
@@ -1,11 +1,40 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, Image, StyleSheet, Text, View } from 'react-native';
+import * as ImagePicker from 'expo-image-picker';
+import { useState } from 'react';
 
 export default function App() {
+  const [image, setImage] = useState(null);
+   
+  const pickImage = async () => {
+    // No permissions request is necessary for launching the image library
+    let result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ImagePicker.MediaTypeOptions.All,
+      allowsEditing: true,
+      aspect: [4, 3],
+      quality: 1,
+    });
+
+    console.log(result);
+
+    if (!result.canceled) {
+      setImage(result.assets[0].uri);
+    }
+  };
+  
+  
+  
+  
+  const onPressOpenGallery = () => {
+      pickImage(
+        
+      );
+  }
+  
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+        <Button title="갤러리 열기" onPress={onPressOpenGallery}/>
+        <Image source={{uri: image}} /> 
     </View>
   );
 }
@@ -14,7 +43,8 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent:"center",
+    alignItems:"center",
+   
   },
 });
